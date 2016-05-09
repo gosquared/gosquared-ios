b052fff1af8207abf6e9b3fdc3fc5e05f4892953
Ed Wellbrook <edwellbrook@gmail.com>
2016-05-09 15:23:56 +0100

    update copyright dates

--- GoSquared/GSRequest.m
+++ GoSquared/GSRequest.m
@@ -4,7 +4,7 @@
 //
 //  Created by Giles Williams on 12/10/2014.
 //  Copyright (c) 2014 Urban Massage. All rights reserved.
-//  Copyright (c) 2015 Go Squared Ltd. All rights reserved.
+//  Copyright (c) 2015-2016 Go Squared Ltd. All rights reserved.
 //
 
 #import "GSRequest.h"
