e1b7033d6cff4c811ebb82cc2790e2869acf443a
Ed Wellbrook <edwellbrook@gmail.com>
2016-05-25 11:48:15 +0100

    update header comment

--- GoSquared+Chat/Models/GSChatConnectionStatus.h
+++ GoSquared+Chat/Models/GSChatConnectionStatus.h
@@ -1,9 +1,9 @@
 //
 //  GSChatConnectionStatus.h
-//  Pods
+//  GoSquared
 //
 //  Created by Edward Wellbrook on 20/05/2016.
-//
+//  Copyright © 2016 Go Squared Ltd. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
